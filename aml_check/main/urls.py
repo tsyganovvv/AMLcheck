7461756540aad07cbc3112b205b903fb1ad2001c
Vladimir Tsyganov <153202831+tsyganovvv@users.noreply.github.com>
2025-04-09 20:09:09 +0700

    check + index draft

--- aml_check/main/urls.py
+++ aml_check/main/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 
-from main.views import index
+from main.views import index, check
 
 
 app_name = 'main'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('check', check, name='check'),
 ]
--- aml_check/main/views.py
+++ aml_check/main/views.py
@@ -7,3 +7,10 @@ def index(request):
         'user': '1'
     }
     return render(request, 'main/index.html', context)
+
+
+def check(request):
+    context = {
+        'user': '1'
+    }
+    return render(request, 'main/check.html', context)
